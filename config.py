e63c0b6f3c5917ed6ef8a1294cf4094372cbc509
enigmaticcat <tranquangminh_hsgs19@hus.edu.vn>
2026-04-19 22:54:21 +0700

    hh

--- config.py
+++ config.py
@@ -4,16 +4,17 @@ Shared path config — chỉ cần sửa file này khi chuyển sang máy mới.
 import os
 
 # ── Đổi dòng này khi clone sang máy/server khác ───────────────────────────────
-BASE_DIR = "/Users/nguyenthithutam/Desktop/mtdna_rerun"
-
+# BASE_DIR = "/Users/nguyenthithutam/Desktop/mtdna_rerun"
+BASE_DIR = "/kaggle/working/mtdna_rerun"
 # Path gốc được hardcode trong train/test manifest (để remap json_path)
 OLD_BASE = "/home/minhtq/mtDNA_proj/mtdna_rerun"
 
+BASE1_DIR = "/kaggle/input/datasets/arrebol314/data-origin"
 # ── Các path derived — không cần chỉnh ────────────────────────────────────────
-PIPELINE_DIR  = os.path.join(BASE_DIR, "pipeline_results")
+PIPELINE_DIR  = os.path.join(BASE1_DIR, "pipeline_results")
 METADATA_TSV  = os.path.join(BASE_DIR, "metadata_rerun.tsv")
 OUTPUT_DIR    = os.path.join(BASE_DIR, "classifier_output")
-DATASET_DIR   = os.path.join(BASE_DIR, "dataset")
+DATASET_DIR   = os.path.join(BASE1_DIR, "dataset")
 FEATURES_CSV  = os.path.join(OUTPUT_DIR, "features.csv")
 TRAIN_CSV     = os.path.join(DATASET_DIR, "train_manifest.csv")
 TEST_CSV      = os.path.join(DATASET_DIR, "test_manifest.csv")
